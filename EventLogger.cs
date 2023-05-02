ec2582fe07f1b94ea234f3144a177d20152fb2c7
Saeed Molaee <saeedmolaiy.dev@gmail.com>
2023-05-02 15:17:47 +0330

    Create EventLogger.cs

---
+++ EventLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace EventLogger.Models;
+
+public static class EventLogger
+{
+    private const string Source = "MyApp";
+    private const string Log = "Application";
+
+    public static void LogError(string message) => LogEvent(EventLogEntryType.Error, message);
+
+    public static void LogWarning(string message) => LogEvent(EventLogEntryType.Warning, message);
+
+    public static void LogInformation(string message) => LogEvent(EventLogEntryType.Information, message);
+
+    private static void LogEvent(EventLogEntryType type, string message)
+    {
+        if (!EventLog.SourceExists(Source))
+        {
+            EventLog.CreateEventSource(Source, Log);
+        }
+
+        EventLog.WriteEntry(Source, message, type);
+    }
+}
